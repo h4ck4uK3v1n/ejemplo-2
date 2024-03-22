f27b027c7616677722e7c9dd86cd13b0ffe1c2db
Kevin Alexis Rodriguez Condori <sis414.kevinrodriguez17@gmail.com>
2024-03-22 00:16:13 -0400

    commit desde dev

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-const a = 20
-let b = 30
+// write a function to fetch the data from the API placeholder
+// and return the data in the format of an array of objects
+// where each object is a user
+// each user should have the following keys:
+// id, name, username, email, address, phone, website, company
+// address should have the following keys:
+// street, suite, city, zipcode, geo
+// geo should have the following keys:
+// lat, lng
+// company should have the following keys:
+// name, catchPhrase, bs
 
-console.log(a + b)
+const fetchUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+    return data.map(user => {
+        return {
+            id: user.id,
+            name: user.name,
+            username: user.username,
+            email: user.email,
+            address: {
+                street: user.address.street,
+                suite: user.address.suite,
+                city: user.address.city,
+                zipcode: user.address.zipcode,
+                geo: {
+                    lat: user.address.geo.lat,
+                    lng: user.address.geo.lng
+                }
+            },
+            phone: user.phone,
+            website: user.website,
+            company: {
+                name: user.company.name,
+                catchPhrase: user.company.catchPhrase,
+                bs: user.company.bs
+            }
+        }
+    });
+}
+
+fetchUsers().then(data => console.log(data));
