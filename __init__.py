bdaf114a5e1fb311770ef46dfe7afbcf5e5cba08
h4ck4uK3v1n <164458258+h4ck4uK3v1n@users.noreply.github.com>
2024-03-22 02:19:21 -0400

    upload files

---
+++ __init__.py
@@ -0,0 +1,8 @@
+def saludar():
+    print("Hola, soy un módulo")
+
+
+# Path: src/main.py
+
+if __name__ == '__main__':
+    saludar()
---
+++ app.js
@@ -0,0 +1,20 @@
+
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
